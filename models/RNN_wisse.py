48604fa954e9a50cc9d5d4e3f196a432cbac8116
Wisse Barkhof <wisse-barkhof@hotmail.com>
2018-11-12 13:27:55 +0100

    initial model

---
+++ models/RNN_wisse.py
@@ -0,0 +1,7 @@
+# RNN Layer by Wisse
+
+def class RNNWisse(object):
+    """docstring for RNN layer by Wisse."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
